501dabd427f1db4f456b542f0eb742588ba1b69d
azarashi-jj <164627982+azarashi-jj@users.noreply.github.com>
2024-03-27 01:33:09 +0900

    Add files via upload

--- dentaku.js
+++ dentaku.js
@@ -1,17 +1,48 @@
 function appendToDisplay(value) {
-    var imputValue = document.getElementById("display").value += value;
+    var number = document.getElementById('display').value;
+
+    if(number == "0" && value == 0){
+
+    }else if(value == '+' || value == '-' || value == '*' || value == '/'){
+        const array = number;
+        var last = array.slice(-1)[0];
+        // alert(last);
+        if(last == '+' || last == '-' || last == '*' || last == '/'){
+            // alert("No");
+        }else{
+            document.getElementById("display").value += value;
+        }
+    }else{
+        document.getElementById("display").value += value;
+    }
+}
+
+function backDisplay(){
+    var number = document.getElementById('display').value;
+    const array = number;
+    if(array.length > 0){
+        document.getElementById("display").value = array.slice(0,-1);
+    }  
 }
 
 function clearDisplay(){
-    document.getElementById('display').value = " ";
+    document.getElementById('display').value = null;
 
 }
 
 function calculateResult() {
     try {
         const result = eval(document.getElementById('display').value);
-        document.getElementById('display').value = result;
-    } catch (error) {
+        if(result == null){
+
+        }else if(isFinite(result)){
+            document.getElementById('display').value = result;
+        }else{
+            document.getElementById('display').value = "Error";
+        }
+        
+    }  catch (error) {
         document.getElementById('display').value = 'Error';
     }
+    
 }
